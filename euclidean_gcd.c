b2773348b6787defa960dc9ae867981cd233f0a0
Epik-Whale463 <pvrcharan2022@gmail.com>
2024-09-09 08:19:02 -0400

    add iterative apporach and swap()

--- euclidean_gcd.c
+++ euclidean_gcd.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /* 
 two integers a and b are passed to gcd(a,b) and this happens
 we recursively call gcd(b, a mod b) until the value of b is 0 
 at which point , that current a value is the GCD we are looking for.
 */
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int euclidean_gcd(int a, int b){
     if (b == 0) 
@@ -13,13 +19,25 @@ int euclidean_gcd(int a, int b){
         euclidean_gcd(b, a % b);
 }
 
+int gcd_non_recursive(int a, int b){
+    while(b){
+        a = a %b;
+        swap(&a,&b);
+    }
+    return a;
+}
+
 int main(void){
 
     int a, b;
     scanf("%d",&a);
     scanf("%d",&b);
     int gcd = euclidean_gcd(a,b);
+    int gcd_non_rec = gcd_non_recursive(a,b);
+
     printf("The GCD of %d and %d is : %d\n",a,b,gcd);
+    printf("(non recursive) The GCD of %d and %d is : %d\n",a,b,gcd_non_rec);
+
 
     return 0;
 }
